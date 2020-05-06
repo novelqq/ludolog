652bfbadd3dee24f2c119e2f52c10c2386a3105e
ryan <ryan@lain.wired>
2020-05-05 17:08:34 -0700

    very rudimentary user authentication using JWT

--- backend/backludo/urls.py
+++ backend/backludo/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('token-auth/', obtain_jwt_token)
 ]
